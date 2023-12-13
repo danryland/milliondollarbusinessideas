70a35d6f4153aea6054842d0a301ae26bc6f47e7
Dan Ryland <dan@ryland.consulting>
2023-12-13 18:31:04 +0000

    add simple home page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,32 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="text-center q-pa-lg">
+      <h1>
+        <span class="top">Build in a weekend</span><br /><span class="bottom"
+          >Make millions</span
+        >
+      </h1>
+      <p>
+        An
+        <a
+          href="https://github.com/danryland/milliondollarbusinessideas"
+          target="_blank"
+          >open source</a
+        >
+        project by<br />
+        <a target="_blank" href="https://twitter.com/realdanryland"
+          >Dan Ryland</a
+        >, co-founder of
+        <a target="_blank" href="https://insidrmusic.com">Insidr&nbsp;Music</a>
+      </p>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
