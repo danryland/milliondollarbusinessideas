d193a837446de5af9132f3503c221fa90e82aa28
Dan Ryland <dan@ryland.consulting>
2023-12-17 01:40:14 +0000

    add edge function and bespoke ideas

--- src/pages/IdeasPage.vue
+++ src/pages/IdeasPage.vue
@@ -2,71 +2,108 @@
   <q-page class="flex flex-center">
     <div class="text-center q-pa-md flex column flex-center">
       <div class="q-mb-lg">
-        <h1 class="text-h4 q-mb-sm">Choose an idea</h1>
-        <p class="text-subtitle1 text-grey-7" v-if="ideas.length">
+        <h1 class="text-h4 q-mb-sm" v-if="ideas.length">Choose an idea</h1>
+        <p class="text-subtitle1 text-grey-7" v-if="!interest && ideas.length">
           From our
           <strong>{{ totalIdeas }} <i>million dollar ideas</i></strong
           >,<br />here's
           <strong>{{ activeIdea + 1 }} of {{ ideas.length }}</strong> to get
           your started:
         </p>
-        <div v-if="interest">
+        <p class="text-subtitle1 text-grey-7" v-if="interest && ideas.length">
           From our
           <strong>{{ totalIdeas }} <i>million dollar ideas</i></strong
           >,<br />here's
-          <strong>{{ activeIdea }} of {{ ideas.length }}</strong> ideas linked
-          to <strong>{{ interest }}</strong>
-        </div>
+          <strong>{{ activeIdea + 1 }} of {{ ideas.length }}</strong> ideas
+          linked to <strong>{{ interest }}</strong>
+        </p>
       </div>
       <div class="note-holder">
-        <div v-if="interest">
-          <q-card class="note note-1">
-            <q-card-section>
-              <p>Coming soon</p>
-            </q-card-section>
-          </q-card>
+        <div v-if="!ideas.length">
+          <q-spinner size="32px" class="q-mb-md" />
+          <p class="text-subtitle1 text-grey-7">
+            <strong>Generating ideas...</strong><br /><small
+              >This can take up 10-20s to complete</small
+            >
+          </p>
         </div>
-        <div v-for="(idea, index) in ideas" :key="idea.id">
-          <transition
-            appear
-            :enter-active-class="
-              [
-                'animated slower bounceInLeft',
-                'animated slower bounceInDown',
-                'animated slower bounceInRight',
-                'animated slower bounceInUp',
-              ][Math.floor(Math.random() * 4)]
-            "
-            :leave-active-class="
-              [
-                'animated slow bounceOutLeft',
-                'animated slow bounceOutDown',
-                'animated slow bounceOutRight',
-                'animated slow bounceOutUp',
-              ][Math.floor(Math.random() * 4)]
-            "
-          >
-            <q-card
-              v-if="index <= activeIdea"
-              :class="['note', 'note-' + (index + 1)]"
-              :style="{ zIndex: index }"
-              @click="
-                $router.push({
-                  name: 'idea',
-                  params: {
-                    id: idea.id,
-                    slug: idea.title
-                      .replace(/[^a-zA-Z0-9]/g, '-')
-                      .toLowerCase(),
-                  },
-                })
+        <div v-if="interest">
+          <div v-for="(idea, index) in ideas" :key="idea.id">
+            <transition
+              appear
+              :enter-active-class="
+                [
+                  'animated slower bounceInLeft',
+                  'animated slower bounceInDown',
+                  'animated slower bounceInRight',
+                  'animated slower bounceInUp',
+                ][Math.floor(Math.random() * 4)]
+              "
+              :leave-active-class="
+                [
+                  'animated slow bounceOutLeft',
+                  'animated slow bounceOutDown',
+                  'animated slow bounceOutRight',
+                  'animated slow bounceOutUp',
+                ][Math.floor(Math.random() * 4)]
               "
             >
-              <q-card-section>
-                <p>{{ idea.description }}</p>
-              </q-card-section>
-            </q-card>
-          </transition>
+              <q-card
+                v-if="index <= activeIdea"
+                :class="['note', 'note-' + (index + 1)]"
+                :style="{ zIndex: index }"
+                @click="showAlert()"
+              >
+                <q-card-section>
+                  <p>{{ idea.description }}</p>
+                </q-card-section>
+              </q-card>
+            </transition>
+          </div>
+        </div>
+        <div v-else>
+          <div v-for="(idea, index) in ideas" :key="idea.id">
+            <transition
+              appear
+              :enter-active-class="
+                [
+                  'animated slower bounceInLeft',
+                  'animated slower bounceInDown',
+                  'animated slower bounceInRight',
+                  'animated slower bounceInUp',
+                ][Math.floor(Math.random() * 4)]
+              "
+              :leave-active-class="
+                [
+                  'animated slow bounceOutLeft',
+                  'animated slow bounceOutDown',
+                  'animated slow bounceOutRight',
+                  'animated slow bounceOutUp',
+                ][Math.floor(Math.random() * 4)]
+              "
+            >
+              <q-card
+                v-if="index <= activeIdea"
+                :class="['note', 'note-' + (index + 1)]"
+                :style="{ zIndex: index }"
+                @click="
+                  $router.push({
+                    name: 'idea',
+                    params: {
+                      id: idea.id,
+                      slug: idea.title
+                        .replace(/[^a-zA-Z0-9]/g, '-')
+                        .toLowerCase(),
+                    },
+                  })
+                "
+              >
+                <q-card-section>
+                  <p>{{ idea.description }}</p>
+                </q-card-section>
+              </q-card>
+            </transition>
+          </div>
         </div>
       </div>
     </div>
@@ -94,6 +131,7 @@
         :disable="activeIdea > getLength(ideas) - 1"
       />
       <q-btn
+        v-if="!interest"
         size="lg"
         round
         color="primary"
@@ -111,6 +149,15 @@
           })
         "
       />
+      <q-btn
+        v-if="interest"
+        size="lg"
+        round
+        color="primary"
+        text-color="dark"
+        icon="fa-sharp fa-solid fa-check"
+        @click="showAlert()"
+      />
     </q-footer>
   </q-page>
 </template>
@@ -119,6 +166,7 @@
 import { ref, onMounted } from "vue";
 import supabase from "../supabase";
 import { useRoute } from "vue-router";
+import axios from "axios";
 
 export default {
   name: "IdeasPage",
@@ -178,13 +226,38 @@ export default {
       return 0;
     };
 
+    const generateCustomIdeas = async (interestValue) => {
+      isLoading.value = true;
+      try {
+        const response = await axios.post(
+          `${process.env.SUPABASE_FUNCTION}/bespoke-idea`,
+          { interest: interestValue.value },
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.SUPABASE_KEY}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        ideas.value = JSON.parse(
+          response.data.replace("```json", "").replace("```", "").trim()
+        );
+        console.log("Bespoke ideas: ", ideas.value);
+      } catch (error) {
+        console.error("Error fetching custom ideas:", error);
+      } finally {
+        isLoading.value = false;
+      }
+    };
+
     onMounted(async () => {
       await getTotalIdeas();
       interest.value = route.query.interest;
       console.log(route);
       if (interest.value) {
         console.log(interest);
-        //await generateCustomIdeas(interest);
+        await generateCustomIdeas(interest);
       } else {
         await getIdeas();
       }
--- supabase/functions/bespoke-idea/index.ts
+++ supabase/functions/bespoke-idea/index.ts
@@ -2,6 +2,7 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
+import "https://deno.land/x/xhr@0.3.0/mod.ts";
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { Configuration, OpenAIApi } from "https://esm.sh/openai@3.2.1";
 
@@ -22,32 +23,44 @@ serve(async (req) => {
     });
   }
 
-  console.log("Config: ", configuration);
-
-  // let config = {
-  //   method: 'get',
-  //   maxBodyLength: Infinity,
-  //   url: 'https://api.search.brave.com/res/v1/news/search?q=',
-  //   headers: {
-  //     'Accept': 'application/json',
-  //     'Accept-Encoding': 'gzip',
-  //     'X-Subscription-Token': Deno.env.get("BRAVE_API")
-  //   }
-  // };
-
-  // axios.request(config)
-  // .then((response) => {
-  //   console.log(JSON.stringify(response.data));
-  // })
-  // .catch((error) => {
-  //   console.log(error);
-  // });
-
   try {
     const requestBody = await req.json();
 
-    const systemPrompt = `Get me 5 million dollar business ideas using the following json format: title, description, mvp, first_users and first_million`;
-    const userPrompt = ``;
+    const userInterest = requestBody.interest;
+
+    console.log("Interest: ", userInterest);
+
+    const braveSearchResponse = await fetch(
+      `https://api.search.brave.com/res/v1/news/search?q=${userInterest}`,
+      {
+        headers: {
+          "X-Subscription-Token": Deno.env.get("BRAVE_API"),
+        },
+      }
+    );
+    const newsData = await braveSearchResponse.json();
+    const newsArray = newsData.results.map((result) => {
+      return {
+        title: result.title,
+        description: result.description,
+      };
+    });
+
+    console.log("News: ", newsArray);
+
+    const systemPrompt = `Based on the user's interest and search results, give me 5 "million dollar business ideas" that follow the following pattern: Title, Description, MVP, Get first 10 users, Get first million. Give the response in pure JSON, not wrapped in markdown, so it can be reused:
+    [{
+      "title": "",
+      "description": "",
+      "mvp": "",
+      "first_users": "",
+      "first_million": "",
+    }]`;
+    const userPrompt = `Interest: ${userInterest}
+    Search results: ${JSON.stringify(newsArray, null, 2)}`;
+
+    console.log("system: ", systemPrompt);
+    console.log("user: ", userPrompt);
 
     const response = await openai.createChatCompletion({
       model: "gpt-4-1106-preview",
@@ -55,7 +68,7 @@ serve(async (req) => {
         { role: "system", content: systemPrompt },
         { role: "user", content: userPrompt },
       ],
-      max_tokens: 500,
+      max_tokens: 2000,
     });
 
     const {
@@ -85,7 +98,7 @@ serve(async (req) => {
 // curl -i --location --request POST 'http://localhost:54321/functions/v1/bespoke-idea' \
 //   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
 //   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
+//   --data '{"interest":"Celtic drumming"}'
 
 // API URL: http://localhost:54321
 // GraphQL URL: http://localhost:54321/graphql/v1
